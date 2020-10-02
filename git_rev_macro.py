49a42e2875562a5ff6af381f3a3816c0ce796d94
markmellors <mark.mellors@gmail.com>
2020-10-02 23:27:10 +0100

    display git branch and rev on screen at startup

--- git_rev_macro.py
+++ git_rev_macro.py
@@ -5,4 +5,11 @@ revision = (
     .strip()[:7]
     .decode("utf-8")
 )
-print("-DGIT_REV='\"%s\"'" % revision)
+branch = (
+    subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"])
+    .strip()[-20::1]
+    .decode("utf-8")
+)
+
+#print("-DGIT_BRANCH='\"%s\"'" % revision)
+print("-DGIT_REV='\"%s\"' -DGIT_BRANCH='\"%s\"'" % (revision, branch))
