959ef3920018e047020f416bb73b6b651ca13032
Rob Berwick <rob.berwick@gmail.com>
2020-10-03 15:40:15 +0100

    Remove dead code

--- git_rev_macro.py
+++ git_rev_macro.py
@@ -1,9 +1,7 @@
 import subprocess
 
 revision = (
-    subprocess.check_output(["git", "rev-parse", "HEAD"])
-    .strip()[:7]
-    .decode("utf-8")
+    subprocess.check_output(["git", "rev-parse", "HEAD"]).strip()[:7].decode("utf-8")
 )
 branch = (
     subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"])
@@ -11,5 +9,4 @@ branch = (
     .decode("utf-8")
 )
 
-#print("-DGIT_BRANCH='\"%s\"'" % revision)
 print("-DGIT_REV='\"%s\"' -DGIT_BRANCH='\"%s\"'" % (revision, branch))
