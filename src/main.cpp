ac21668a0fc38d1096b88a562e1248a560a69574
markmellors <mark.mellors@gmail.com>
2020-10-25 17:00:38 +0000

    some tidying plus extra debugging prints

--- src/main.cpp
+++ src/main.cpp
@@ -724,9 +724,6 @@ void loop() {
         processMessage(myTransfer);
     }
 
-    int messageSize=0;
-    messageSize = myTransfer.packet.bytesToRec;
-
 
     // if the message sending timeout has passed then increment the missed count
     // and reset
@@ -740,8 +737,7 @@ void loop() {
 
     display.clearDisplay();
     display.setCursor(0, 0);
-    display.printf("mess size: %d", messageSize);
-    display.println(" ");
+
     if (missedMotorMessageCount >= 10) {
         shouldInvertDisplay = true;
         display.printf("missed message %d", missedMotorMessageCount);
@@ -815,4 +811,9 @@ void loop() {
         // Send data
         myTransfer.sendData(payloadSize);
     }
+    display.printf("sensor2: %2.2f", distances[2]);
+    display.println(" ");
+    display.printf("sensor 3: %2.2f", distances[3]);
+    display.display();
+
 }
